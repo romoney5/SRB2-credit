337a69beb84de954039b6dfc1ef812a1027d950f
Eidolon <furyhunter600@gmail.com>
2024-07-13 14:48:58 -0500

    Don't define min/max in C++

--- src/doomdef.h
+++ src/doomdef.h
@@ -624,11 +624,13 @@ UINT32 quickncasehash (const char *p, size_t n)
 	return x;
 }
 
+#ifndef __cplusplus
 #ifndef min // Double-Check with WATTCP-32's cdefs.h
 #define min(x, y) (((x) < (y)) ? (x) : (y))
 #endif
 #ifndef max // Double-Check with WATTCP-32's cdefs.h
 #define max(x, y) (((x) > (y)) ? (x) : (y))
+#endif
 #endif
 
 // Max gamepad/joysticks that can be detected/used.
