30b232a7c32216e523a3bfc0cc24aaeaeb58562b
chromaticpipe <127907268+chromaticpipe@users.noreply.github.com>
2025-03-19 17:32:04 -0500

    Add GL Spriteshadows, ported from 2.1

--- src/r_main.c
+++ src/r_main.c
@@ -142,6 +142,7 @@ static CV_PossibleValue_t menubg_cons_t[] = {
 	{187, "Magenta"}, {199, "Lavender"}, {207, "Rosy"}, {215, "Lilac"},
 	{238, "Brown"}, {251, "Beige"}, {0, NULL}
 };
+static CV_PossibleValue_t shadow_cons_t[] = {{0, "Off"}, {1, "Drop"}, {2, "Sprite"}};
 
 static void R_SetFov(fixed_t playerfov);
 
@@ -160,7 +161,7 @@ consvar_t cv_menubgcolor = CVAR_INIT ("menubgcolor", "Blue", CV_SAVE|CV_CLIENT, 
 consvar_t cv_flipcam = CVAR_INIT ("flipcam", "No", CV_SAVE|CV_CALL|CV_NOINIT, CV_YesNo, FlipCam_OnChange);
 consvar_t cv_flipcam2 = CVAR_INIT ("flipcam2", "No", CV_SAVE|CV_CALL|CV_NOINIT, CV_YesNo, FlipCam2_OnChange);
 
-consvar_t cv_shadow = CVAR_INIT ("shadow", "On", CV_SAVE, CV_OnOff, NULL);
+consvar_t cv_shadow = CVAR_INIT ("shadow", "On", CV_SAVE, shadow_cons_t, NULL);
 consvar_t cv_skybox = CVAR_INIT ("skybox", "On", CV_SAVE, CV_OnOff, NULL);
 consvar_t cv_allowmlook = CVAR_INIT ("allowmlook", "Yes", CV_NETVAR|CV_ALLOWLUA, CV_YesNo, NULL);
 consvar_t cv_showhud = CVAR_INIT ("showhud", "Yes", CV_CALL|CV_ALLOWLUA,  CV_YesNo, R_SetViewSize);
