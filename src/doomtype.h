382235994d8ff60c0198336e5665f5ca36745768
SteelT <steeltitanium1@gmail.com>
2026-02-04 21:50:39 -0500

    Fix compiling of non-Windows builds

--- src/doomtype.h
+++ src/doomtype.h
@@ -145,7 +145,13 @@ size_t strlcpy(char *dst, const char *src, size_t siz);
 #include <stdbool.h>
 #endif
 
+#ifdef _WIN32
+#ifndef __cplusplus
 #define boolean bool
+#endif
+#else
+typedef bool boolean;
+#endif
 
 /* 7.18.2.1  Limits of exact-width integer types */
 
