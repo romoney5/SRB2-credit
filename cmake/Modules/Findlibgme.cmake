a897f659bace639e26adb6a060668b057789bf3a
romoney5 <93959424+romoney5@users.noreply.github.com>
2026-07-11 11:44:28 -0400

    Update Findlibgme.cmake 9

--- cmake/Modules/Findlibgme.cmake
+++ cmake/Modules/Findlibgme.cmake
@@ -13,12 +13,11 @@ find_path(libgme_INCLUDE_DIR
 )
 
 find_library(libgme_LIBRARY
-	NAMES gme
+	NAMES gme libgme.0.6.5.dylib
 	PATHS
 		${libgme_PKGCONF_LIBRARY_DIRS}
 		"/usr/lib"
 		"/usr/local/lib"
-		"/usr/local/include"
 )
 
 include(FindPackageHandleStandardArgs)
